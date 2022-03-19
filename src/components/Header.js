8833bb0f2f14543552388e4cbf475def8b5888c4
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2022-03-18 22:37:21 -0500

    Clearing some useless code lines

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,11 @@ export default class Header extends Component {
       background-image: url(https://raw.githubusercontent.com/CarlinGebyte/Reto-1-Sprint-2-AG/499ba5fae3f7afbcd3c23182d0c0ee6450b4c562/src/img/bg-header-desktop.svg);
       height: 25.2vh;
       background-repeat: no-repeat;
+      background-size: cover;
       width: 100%;
     `;
-    const ImgHeader = styled.img`
-    
-    `;
     return (
-      <Header>
-      </Header>
+      <Header/>
     );
   }
 }
