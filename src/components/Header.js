f79b4635e2e52d88315d27bb05b9834b374b3c4b
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2022-03-18 22:22:08 -0500

    Creating Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
 
 export default class Header extends Component {
   render() {
+    const Header = styled.div`
+      background-color: hsl(180, 29%, 50%);
+      background-image: url(https://raw.githubusercontent.com/CarlinGebyte/Reto-1-Sprint-2-AG/499ba5fae3f7afbcd3c23182d0c0ee6450b4c562/src/img/bg-header-desktop.svg);
+      height: 25.2vh;
+      background-repeat: no-repeat;
+      width: 100%;
+    `;
+    const ImgHeader = styled.img`
+    
+    `;
     return (
-      <header>Header</header>
-    )
+      <Header>
+      </Header>
+    );
   }
 }
